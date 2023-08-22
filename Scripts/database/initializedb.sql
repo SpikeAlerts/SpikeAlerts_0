2b0c90955ec54e8bf0ac8babcd81ae822f0f1b78
RwHendrickson <91440903+RwHendrickson@users.noreply.github.com>
2023-08-21 23:13:34 -0500

    Update initializedb.sql
    Adding new tables to initializedb.sql

--- Scripts/database/initializedb.sql
+++ Scripts/database/initializedb.sql
@@ -1,3 +1,16 @@
+-- Run this file to initialize the database, schema, and tables for PurpleAir Monitor Readings in Minneapolis
+-- You can run this by using a psql command like:
+-- psql "host=postgres.cla.umn.edu user=<your_username> password=<your_password> " -f initialize_db.sql
+
+-- To Do beforehand
+
+--CREATE DATABASE "SpikeAlerts"; -- Create the database
+
+--\c "SpikeAlerts"; -- Connect to database This needs a password!
+
+--CREATE EXTENSION postgis; -- Add spatial extensions
+--CREATE EXTENSION postgis_topology;
+
 CREATE table "Sign Up Information"
 	(user_index serial, first_name text, last_name text,
 	 intersection_index int, phone_number int, email text, 
@@ -14,3 +27,56 @@ CREATE table "Archived Alerts Acute PurpleAir"
 ALTER TABLE "Sign Up Information"
   ALTER COLUMN phone_number TYPE bigint;
 
+CREATE TABLE "Minneapolis Boundary"
+(
+    "CTU_ID" int, -- Unique Identifier
+    "CTU_NAME" text, -- City/Township Name
+    "CTU_CODE" text, -- City/Township Code
+    geometry geometry -- Polygon
+);
+
+CREATE TABLE "PurpleAir Stations" -- See PurpleAir API - https://api.purpleair.com/
+(
+	sensor_index int,
+	firmware_version varchar(30),
+	date_created timestamp,
+	last_modified timestamp, 
+	last_seen timestamp,
+	"name" varchar(100),
+	uptime int,
+	position_rating int,
+	channel_state int,
+	channel_flags int,
+	altitude int,
+	geometry geometry
+);
+
+CREATE TABLE "MNDOT Current AADT Segments" -- Create table to store information on Current AADT segments - https://gisdata.mn.gov/dataset/trans-aadt-traffic-segments
+( 
+    "SEQUENCE_NUMBER" int, -- Unique identifier
+    "ROUTE_LABEL" text,
+    "STREET_NAME" text,
+    "DAILY_FACTOR" text,
+    "SEASONAL_FACTOR" text,
+    "AXLE_FACTOR" text,
+    "CURRENT_YEAR" int,
+    "CURRENT_VOLUME" int,
+    geometry geometry
+);
+
+CREATE TABLE "Road Intersections" -- Create table to store road intersections in minneapolis
+(
+    intersection_index serial, -- Unique identifier
+    "NS_cross_street" text, 
+    "EW_cross_street" text,
+	nearby_sensors int [], -- nearby sensors as an array of integers
+    geometry geometry
+);
+
+--CREATE TABLE "Nearby Intersections"
+--(
+--    intersection_index integer, -- unique identifier
+--    nearby_sensors int [] -- nearby sensors as an array of integers
+--    --nearby_facilities int [], -- nearby mpca permitted emitter
+--    --nearby_segments int [], -- nearby mndot aadt segment    
+--);
