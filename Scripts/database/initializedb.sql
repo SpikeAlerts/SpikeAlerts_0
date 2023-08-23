165395a477cd6655528b8593c6a73c88f89be93b
RwHendrickson <91440903+RwHendrickson@users.noreply.github.com>
2023-08-23 12:55:06 -0500

    Update initializedb.sql
    Added Active Alerts list to Sign Up Information

--- Scripts/database/initializedb.sql
+++ Scripts/database/initializedb.sql
@@ -14,7 +14,7 @@
 CREATE table "Sign Up Information"
 	(user_index serial, first_name text, last_name text,
 	 intersection_index int, phone_number int, email text, 
-	 opt_in_phone boolean, opt_in_email boolean, last_messaged timestamp);
+	 opt_in_phone boolean, opt_in_email boolean, last_messaged timestamp, active_alerts bigint []);
 
 CREATE table "Active Alerts Acute PurpleAir"
 	(alert_index bigserial, sensor_index int, start_time timestamp, max_reading float);
