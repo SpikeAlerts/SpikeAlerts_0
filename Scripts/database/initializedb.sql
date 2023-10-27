b62d150ac9c60797d3751f763c0365db7686a6ae
RwHendrickson <91440903+RwHendrickson@users.noreply.github.com>
2023-10-27 12:39:01 -0500

    Database Design Update
    Finalized ER Diagram for forseeable future

--- Scripts/database/initializedb.sql
+++ Scripts/database/initializedb.sql
@@ -11,14 +11,23 @@
 --CREATE EXTENSION postgis; -- Add spatial extensions
 --CREATE EXTENSION postgis_topology;
 
-SET timezone TO 'Europe/Berlin';
-
-CREATE table "Sign Up Information"
-	(record_id serial, -- Unique Identifier from REDCap
-	last_messaged timestamp DEFAULT CURRENT_TIMESTAMP AT TIME ZONE 'America/Chicago', -- Last time messaged
+CREATE table "Sign Up Information" -- This is our internal record keeping for users
+	(record_id integer, -- Unique Identifier from REDCap
+	last_messaged timestamp DEFAULT CURRENT_TIMESTAMP, -- Last time messaged
 	messages_sent int DEFAULT 1, -- Number of messages sent
 	active_alerts bigint [] DEFAULT array[]::bigint [], -- List of Active Alerts
+	cached_alerts bigint [] DEFAULT array[]::bigint [], -- List of ended Alerts not yet notified about
+	subscribed boolean DEFAULT TRUE, -- Is the user wanting texts? 
 	geometry geometry);
+	
+CREATE table "Reports Archive" -- These are for reporting to the City and future research
+	(report_id varchar(12), -- Unique Identifier with format #####-MMDDYY
+	start_time timestamp,
+	duration_minues integer,
+	max_reading float, 
+	sensor_indices int [], -- List of Sensor Unique Identifiers
+	alert_indices bigint [] -- List of Alert Identifiers
+    );
 
 CREATE table "Active Alerts Acute PurpleAir"
 	(alert_index bigserial, -- Unique identifier for an air quality spike alert
@@ -49,7 +58,7 @@ CREATE TABLE "PurpleAir Stations" -- See PurpleAir API - https://api.purpleair.c
 	geometry geometry
 );
 
-CREATE TABLE "Minneapolis Boundary"
+CREATE TABLE "Minneapolis Boundary" -- From MN Geocommons - https://gisdata.mn.gov/dataset/us-mn-state-metc-bdry-census2020counties-ctus
 (
     "CTU_ID" int, -- Unique Identifier
     "CTU_NAME" text, -- City/Township Name
@@ -58,7 +67,7 @@ CREATE TABLE "Minneapolis Boundary"
 );
 
 
--- Don't need AADT yet
+-- Don't need AADT/intersections
 
 --CREATE TABLE "MNDOT Current AADT Segments" -- Create table to store information on Current AADT segments - https://gisdata.mn.gov/dataset/trans-aadt-traffic-segments
 --( 
@@ -75,11 +84,11 @@ CREATE TABLE "Minneapolis Boundary"
 
 
 -- For relating the location to users
-CREATE TABLE "Road Intersections" -- Create table to store road intersections in minneapolis
-(
-    intersection_index serial, -- Unique identifier
-    "NS_cross_street" text, 
-    "EW_cross_street" text,
---	nearby_sensors int [], -- nearby sensors as an array of integers
-    geometry geometry
-);
+--CREATE TABLE "Road Intersections" -- Create table to store road intersections in minneapolis
+--(
+--    intersection_index serial, -- Unique identifier
+--    "NS_cross_street" text, 
+--    "EW_cross_street" text,
+----	nearby_sensors int [], -- nearby sensors as an array of integers
+--    geometry geometry
+--);
