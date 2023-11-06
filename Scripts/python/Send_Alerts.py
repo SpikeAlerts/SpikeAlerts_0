cf6b9299c940189660daea8a31db911863c2c88a
RwHendrickson <91440903+RwHendrickson@users.noreply.github.com>
2023-11-06 13:13:54 -0600

    REDCap in Send_Alerts.py
    Incorporated REDCap into get_phone_numbers()

--- Scripts/python/Send_Alerts.py
+++ Scripts/python/Send_Alerts.py
@@ -206,22 +206,70 @@ def send_all_messages(record_ids, messages):
 
 # ~~~~~~~~~~~~~
 
-def get_phone_numbers(users):
+def get_phone_numbers(record_ids, redCap_token_signUp):
     '''
-    takes a list of users and gets associated phone numbers
+    takes a list of record_ids [int] and gets associated phone numbers. 
+    
+    Returns phone_numbers in a list with the same order as record_ids
+    
+    ** Right now, phone numbers come in the format '(XXX) XXX-XXXX'
     
     Assumption: there will always be a valid phone number in REDcap data. Otherwise we would need to add error handling in send_all_messages
     '''
-
-    print("getting numbers!")
     
-    numbers = []
-    for record_id in users:
-        ### needs some contact with REDCap. API? 
-        # per ERD shouldn't have any interaction w/ Sign Up Info
-        numbers.append(int(record_id*100))
+    # Initialize return value
     
-    return numbers
+    phone_numbers = []
+    
+    ## Prep the REDCap Query
+    
+    # For how to do this - https://education.arcus.chop.edu/redcap-api/
+    # Redcap logic guide - https://cctsi.cuanschutz.edu/docs/librariesprovider28/redcap/redcap-logic-guide.pdf?sfvrsn=258e94ba_2
+    # Info from PyCap - https://redcap-tools.github.io/PyCap/api_reference/records/
+    
+    # Select by record_id <- probably not the best way, but I couldn't get 'record' to work properly in data
+    
+    record_id_strs = [str(record_id) for record_id in record_ids]
+    filterLogic_str = '[record_id]=' + ' OR [record_id]='.join(record_id_strs)
+    
+    # Select fields
+    
+    field_names = 'record_id, phone' # Field Names
+    
+    data = {
+    'token': redCap_token_signUp,
+    'content': 'record',
+    'fields' : field_names,
+    'action': 'export',
+    'format': 'csv',
+    'type': 'flat',
+    'csvDelimiter': '',
+    'rawOrLabel': 'raw',
+    'rawOrLabelHeaders': 'raw',
+    'exportCheckboxLabel': 'false',
+    'exportSurveyFields': 'false',
+    'exportDataAccessGroups': 'false',
+    'returnFormat': 'csv',
+    'filterLogic': filterLogic_str  
+    }
+    
+    # Send the request
+    r = requests.post('https://redcap.ahc.umn.edu/api/',data=data)
+    
+    # Unpack the response
+    
+    if r.status_code == 200:
+        
+        df = pd.read_csv(StringIO(r.text)) # Read as a dataframe
+        
+        sorted_df = df.set_index('record_id').loc[record_ids] # Sort df by input record_ids
+        
+        phone_numbers = list(sorted_df.phone) # Extract phone numbers <- currently in format '(XXX) XXX-XXXX'
+        
+    else:
+        print('Error Receiving REDCap data')
+    
+    return phone_numbers
 
 # ~~~~~~~~~~~~~
 
