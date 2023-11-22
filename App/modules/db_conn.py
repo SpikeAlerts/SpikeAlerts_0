28c9d0e231060bb2499b61fa1e117b46da60b72e
Dan Raskin <Danieltraskin@gmail.com>
2023-11-22 13:28:53 -0600

    parse db url

--- App/modules/db_conn.py
+++ App/modules/db_conn.py
@@ -1,12 +1,21 @@
 import os # For working with Operating System
-from urllib.parse import quote
+from urllib.parse import urlparse, quote
 from dotenv import load_dotenv # Loading .env info
 load_dotenv()
 
 if os.getenv('DATABASE_URL'):
-  url = os.getenv('DATABASE_URL')+ '?options=' + quote(os.getenv('DB_OPTIONS'))
-  print(url)
-  pg_connection_dict = url
+  parsed_url = urlparse(os.getenv('DATABASE_URL'))
+  print(parsed_url)
+
+  creds = [parsed_url.path[1:],
+        parsed_url.username,
+        parsed_url.password,
+        parsed_url.hostname,
+        parsed_url.port,
+        quote(os.getenv('DB_OPTIONS'))
+        ]
+  print(creds)
+  pg_connection_dict = dict(zip(['dbname', 'user', 'password', 'port', 'host', 'options'], creds))
 else:
   creds = [os.getenv('DB_NAME'),
           os.getenv('DB_USER'),
