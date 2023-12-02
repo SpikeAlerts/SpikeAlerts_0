1ebe38fcb9f313d686759c66d1916f9a1651bcd7
RwHendrickson <91440903+RwHendrickson@users.noreply.github.com>
2023-12-01 22:02:49 -0600

    Remove Flask App
    This is just the VM that runs the main script.

--- App/aq_spikealerts.py
+++ App/aq_spikealerts.py
@@ -1,39 +1,27 @@
 ## import
 
 import os # For working with Operating System
-import sys
-from dotenv import load_dotenv # Loading .env info
-from flask import Flask 
-from App.modules.db_init import db_init, db_need_init, db_notinit
-from App.modules.MAIN import main_loop
 
+from dotenv import load_dotenv # Loading .env info
+from App.modules.db_init import db_init, db_need_init, db_notinit
+from App.modules import MAIN
+from App.modules import Twilio_Functions as our_twilio
 
 ## variables
 
 load_dotenv() # load .env file
 
-app = Flask(__name__)
-FLASK_HOST=os.getenv('FLASK_HOST')
-FLASK_PORT=os.getenv('FLASK_PORT')
-
-## server
-print("name is", __name__, FLASK_PORT)
-
 if db_need_init() == True:
   print("Minneapolis Boundaries table empty. running db init.")
   db_init()
-main_loop()
 
-# main_loop()
+try:
+    MAIN.main_loop()
+    
+except Exception as e:
+    
+    print(e)
+    
+finally:
 
-@app.route("/")
-def hello_world():
-  return "<!DOCTYPE html><body><p>Hello, dark, dark World!</p><a href='http://localhost:5000/init'><button>init db</button></a><body/></html>"
-
-@app.route("/init")
-def init_db():
-  db_notinit()
-  return "<p>initialized!</p>"
-
-if __name__ == "__main__":
-  app.run(host=FLASK_HOST, port=FLASK_PORT, debug=True)
+    our_twilio.send_texts([os.environ['LOCAL_PHONE']], ['SpikeAlerts Down'])   
