6e30b18f13d097256229796ebec0bb31b4c7dc78
RwHendrickson <91440903+RwHendrickson@users.noreply.github.com>
2023-12-10 10:38:55 -0600

    Database Update
    Modified initializedb.sql to match Heroku version

--- Scripts/database/demo.sql
+++ Scripts/database/demo.sql
@@ -3,12 +3,13 @@
 WITH users as -- Select users to potentially message
 (
 SELECT record_id, geometry
-FROM "Sign Up Information"
+FROM internal."Sign Up Information"
 WHERE record_id = ANY ( ARRAY[1,2,3,4,5] ) -- inserted record_ids
 )
 SELECT u.record_id, MAX(p.last_elevated) as last_elevated
-FROM users u, "PurpleAir Stations" p
-WHERE ST_DWithin(u.geometry, p.geometry, 1000) -- All Sensors within 1 kilometer
+FROM users u, internal."PurpleAir Stations" p
+WHERE ST_DWithin(ST_Transform(u.geometry,26915), -- query for users within the distance from the sensor
+										    ST_Transform(p.geometry, 26915),1000); --1609.34 meters in a mile
 GROUP BY u.record_id;
 
 -- VIEW? Find alerts' last_seen (CTE) <- Could be changed to last_elevated
@@ -16,12 +17,12 @@ GROUP BY u.record_id;
 WITH alerts as -- Find alerts to potentially activate again
 (
 SELECT alert_index, sensor_indices
-FROM "Archived Alerts Acute PurpleAir"
+FROM internal."Archived Alerts Acute PurpleAir"
 WHERE start_time + INTERVAL '1 Minutes' * duration_minutes > 
 			CURRENT_TIMESTAMP AT TIME ZONE 'America/Chicago' - INTERVAL '30 Minutes' -- Alerts ended within 30 minutes
 )
 SELECT a.alert_index, MAX(p.last_seen) as last_seen
-FROM alerts a, "PurpleAir Stations" p
+FROM alerts a, internal."PurpleAir Stations" p
 WHERE p.sensor_index = ANY (a.sensor_indices) -- All Sensors within the sensor_indices
 GROUP BY a.alert_index;
 
@@ -34,43 +35,49 @@ with buff as
 			ST_Transform(s.geometry,26915),
 					1000),
 					4326) as geom 
-	FROM "PurpleAir Stations" s
+	FROM internal."PurpleAir Stations" s
 	WHERE s.channel_state = 3 AND s.channel_flags = 0
 	)
 SELECT ST_UNION(geom) as coverage
 FROM buff;
 
--- Sample users for export (only for testing purposes!)
+-- correcting last_seen
+
+UPDATE internal."PurpleAir Stations"
+SET last_seen = last_seen - interval '20 minutes'
+WHERE channel_flags = 0;
+
+-- Sample users for export (only for test database!)
 select record_id, ST_asText(geometry)
-from "Sign Up Information";
+from internal."Sign Up Information";
 
 -- Look at all the tables
 
 select *
-from "Sign Up Information";
+from internal."Sign Up Information";
 
 select *
-from "PurpleAir Stations";
+from internal."PurpleAir Stations";
 
 select *
-from "PurpleAir Stations"
+from internal."PurpleAir Stations"
 WHERE channel_state = 3;
 
 select *
-from "Active Alerts Acute PurpleAir";
+from internal."Active Alerts Acute PurpleAir";
 
 select *
-from "Archived Alerts Acute PurpleAir";
+from internal."Archived Alerts Acute PurpleAir";
 
 select *
-from "Reports Archive";
+from internal."Reports Archive";
 
 -- For reporting to the City?
 
 with alerts as 
 (
 	SELECT alert_index, sensor_indices[1] as sensor_index, start_time, duration_minutes, max_reading
-	FROM "Archived Alerts Acute PurpleAir"
+	FROM internal."Archived Alerts Acute PurpleAir"
 	WHERE start_time > DATE('2023-11-17')
 )
 SELECT a.alert_index,
@@ -82,7 +89,7 @@ SELECT a.alert_index,
 		ST_X(p.geometry) as longitude, 
 		ST_Y(p.geometry) as latitude
 FROM alerts a
-INNER JOIN "PurpleAir Stations" p ON (p.sensor_index = a.sensor_index);
+INNER JOIN internal."PurpleAir Stations" p ON (p.sensor_index = a.sensor_index);
 
 -- This shows all the active alerts at each station
 
@@ -90,12 +97,11 @@ WITH alerts as
 (
 	SELECT start_time, max_reading, 
 			sensor_indices[1] as sensor_index
-	FROM "Active Alerts Acute PurpleAir"
-	GROUP BY sensor_indices[1]
+	FROM internal."Active Alerts Acute PurpleAir"
 )
-SELECT a.sensor_index, a.start_time, a.max_reading, p.geometry
+SELECT a.sensor_index, a.start_time, p.last_elevated, a.max_reading, p.geometry
 FROM alerts a
-INNER JOIN "PurpleAir Stations" p ON (p.sensor_index = a.sensor_index);
+INNER JOIN internal."PurpleAir Stations" p ON (p.sensor_index = a.sensor_index);
 
 -- This shows a summary of the alerts at each station beyond a specified date
 
@@ -103,25 +109,27 @@ WITH alerts as
 (
 	SELECT COUNT(start_time) as count, ARRAY_AGG(start_time) as start_time, 
 			AVG(duration_minutes) as duration_minutes, 
-			AVG(max_reading) as max_reading, 
+			MAX(max_reading) as max_reading, 
 			sensor_indices[1] as sensor_index
-	FROM "Archived Alerts Acute PurpleAir"
+	FROM internal."Archived Alerts Acute PurpleAir"
 	WHERE start_time > DATE('2023-11-25')
 	GROUP BY sensor_indices[1]
 )
 SELECT a.sensor_index, a.count, a.start_time, a.duration_minutes, a.max_reading, p.geometry
 FROM alerts a
-INNER JOIN "PurpleAir Stations" p ON (p.sensor_index = a.sensor_index);
+INNER JOIN internal."PurpleAir Stations" p ON (p.sensor_index = a.sensor_index);
 
 -- Find users within a distance from a sensor (1km is used, but can change 1000 to 1609.34)
 
 WITH sensor as -- query for the desired sensor
     (
     SELECT sensor_index, geometry
-    FROM "PurpleAir Stations"
+    FROM internal."PurpleAir Stations"
     WHERE sensor_index = 156605
     )
     SELECT *
-    FROM "Sign Up Information" u, sensor s
+    FROM internal."Sign Up Information" u, sensor s
     WHERE u.subscribed = TRUE AND ST_DWithin(ST_Transform(u.geometry,26915), -- query for users within the distance from the sensor
 										    ST_Transform(s.geometry, 26915),1000); --1609.34 meters in a mile
+										    
+										    
--- Scripts/database/initializedb.sql
+++ Scripts/database/initializedb.sql
@@ -8,21 +8,30 @@
 
 --\c "SpikeAlerts"; -- Connect to database This needs a password!
 
+-- CREATE SCHEMA internal;
 --CREATE EXTENSION postgis; -- Add spatial extensions
 --CREATE EXTENSION postgis_topology;
 
-CREATE table "Sign Up Information" -- This is our internal record keeping for users
+CREATE table internal."Daily Log" -- This is to store important daily metrics
+    ("date" date DEFAULT CURRENT_DATE,
+     new_users int,
+     messages_sent int DEFAULT 0,
+     segments_sent int DEFAULT 0,
+	 reports_for_day int DEFAULT 0
+    );
+
+CREATE table internal."Sign Up Information"-- This is our internal record keeping for users
 	(record_id integer, -- Unique Identifier from REDCap
-	last_messaged timestamp DEFAULT CURRENT_TIMESTAMP, -- Last time messaged
+	last_messaged timestamp DEFAULT CURRENT_DATE + INTERVAL '8 hours', -- Last time messaged
 	messages_sent int DEFAULT 1, -- Number of messages sent
 	active_alerts bigint [] DEFAULT array[]::bigint [], -- List of Active Alerts
 	cached_alerts bigint [] DEFAULT array[]::bigint [], -- List of ended Alerts not yet notified about
 	subscribed boolean DEFAULT TRUE, -- Is the user wanting texts? 
 	geometry geometry);
 	
-CREATE INDEX user_gid ON "Sign Up Information" USING GIST(geometry);  -- Create spatial index
+CREATE INDEX user_gid ON internal."Sign Up Information" USING GIST(geometry);  -- Create spatial index
 	
-CREATE table "Reports Archive" -- These are for reporting to the City and future research
+CREATE table internal."Reports Archive"-- These are for reporting to the City and future research
 	(report_id varchar(12), -- Unique Identifier with format #####-MMDDYY
 	start_time timestamp,
 	duration_minutes integer,
@@ -30,21 +39,27 @@ CREATE table "Reports Archive" -- These are for reporting to the City and future
 	sensor_indices int [], -- List of Sensor Unique Identifiers
 	alert_indices bigint [] -- List of Alert Identifiers
     );
+    
+CREATE TABLE internal."Afterhour Reports" -- Storage for messages informing of an alert that ended overnight
+    (
+    record_id integer, -- Unique Identifier from REDCap
+    message text
+    );
 
-CREATE table "Active Alerts Acute PurpleAir"
+CREATE table internal."Active Alerts Acute PurpleAir"
 	(alert_index bigserial, -- Unique identifier for an air quality spike alert
 	 sensor_indices int [] DEFAULT array[]::int [], -- List of Sensor Unique Identifiers 
 	  start_time timestamp,
 	   max_reading float); -- Maximum value registered from all sensors
 
-CREATE table "Archived Alerts Acute PurpleAir" -- Archive of the Above table
+CREATE table internal."Archived Alerts Acute PurpleAir" -- Archive of the Above table
     (alert_index bigint,
     sensor_indices int [], -- List of Sensor Unique Identifiers 
     start_time timestamp,
     duration_minutes integer,
     max_reading float);
     
-CREATE TABLE "PurpleAir Stations" -- See PurpleAir API - https://api.purpleair.com/
+CREATE TABLE internal."PurpleAir Stations" -- See PurpleAir API - https://api.purpleair.com/
 (
 	sensor_index int,
 	date_created timestamp,
@@ -58,39 +73,12 @@ CREATE TABLE "PurpleAir Stations" -- See PurpleAir API - https://api.purpleair.c
 	geometry geometry
 );
 
-CREATE INDEX PurpleAir_gid ON "PurpleAir Stations" USING GIST(geometry);  -- Create spatial index for stations
+CREATE INDEX PurpleAir_gid ON internal."PurpleAir Stations" USING GIST(geometry);  -- Create spatial index for stations
 
-CREATE TABLE "Minneapolis Boundary" -- From MN Geocommons - https://gisdata.mn.gov/dataset/us-mn-state-metc-bdry-census2020counties-ctus
+CREATE TABLE internal."Minneapolis Boundary"-- From MN Geocommons - https://gisdata.mn.gov/dataset/us-mn-state-metc-bdry-census2020counties-ctus
 (
     "CTU_ID" int, -- Unique Identifier
     "CTU_NAME" text, -- City/Township Name
     "CTU_CODE" text, -- City/Township Code
     geometry geometry -- Polygon
 );
-
-
--- Don't need AADT/intersections
-
---CREATE TABLE "MNDOT Current AADT Segments" -- Create table to store information on Current AADT segments - https://gisdata.mn.gov/dataset/trans-aadt-traffic-segments
---( 
---    "SEQUENCE_NUMBER" int, -- Unique identifier
---    "ROUTE_LABEL" text,
---    "STREET_NAME" text,
---    "DAILY_FACTOR" text,
---    "SEASONAL_FACTOR" text,
---    "AXLE_FACTOR" text,
---    "CURRENT_YEAR" int,
---    "CURRENT_VOLUME" int,
---    geometry geometry
---);
-
-
--- For relating the location to users
---CREATE TABLE "Road Intersections" -- Create table to store road intersections in minneapolis
---(
---    intersection_index serial, -- Unique identifier
---    "NS_cross_street" text, 
---    "EW_cross_street" text,
-----	nearby_sensors int [], -- nearby sensors as an array of integers
---    geometry geometry
---);
